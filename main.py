85d7a421d6ea44769406f93d9d1ea693c45cc672
Gui0delp <delpro.guillaume@gmail.com>
2019-07-02 19:48:02 +0200

    Adding the main of the script

---
+++ main.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+"""
+    blabla
+"""
+
+def main():
+    """
+        Main function of the script
+    """
+    pass
+
+if __name__ == "__main__":
+    main()
