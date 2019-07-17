0700b9b195ac940cef001c3c7fc6739d8b15b7f3
Gui0delp <delpro.guillaume@gmail.com>
2019-07-17 18:27:50 +0200

    Rename the file in openfoodfacts

---
+++ openfoodfacts.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+"""
+    Docstring
+"""
+
+def main():
+    """
+        Main function of the script
+    """
+
+    #Je liste les catégories à recupérer category_list
+    #Je boucle sur la liste
+
+
+    pass
+
+if __name__ == "__main__":
+    main()
