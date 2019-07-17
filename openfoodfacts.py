552466cf0a5a47b8a243705ffefc21519cf12927
Gui0delp <delpro.guillaume@gmail.com>
2019-07-17 22:31:48 +0200

    Add system for convert text in json

--- openfoodfacts.py
+++ openfoodfacts.py
@@ -5,6 +5,7 @@
     Docstring
 """
 
+import json
 import requests  #pylint: disable=E0401
 
 def main():
@@ -27,9 +28,10 @@ def main():
 
     #URL https://fr.openfoodfacts.org/categorie/pizza.json
     print("\nTest the connexions with the differents URL...")# pylint: disable=C0325
+    print("url = https://fr.openfoodfacts.org/categorie/[category_element].json")# pylint: disable=C0325
     print("-----------------------------------------------------------------------")# pylint: disable=C0325
-    for element in category_list:
 
+    for element in category_list:
         quiery_file = requests.get("https://fr.openfoodfacts.org/categorie/{a}.json".format(a=element))# pylint: disable=C0301
 
         if quiery_file.status_code == requests.codes.ok:
@@ -39,7 +41,15 @@ def main():
 
     print("-----------------------------------------------------------------------")#pylint: disable=C0325
 
-    print(quiery_file.json)#pylint: disable=C0325
+    #Transfer data into a dictionnary
+    dict_element = json.loads(quiery_file.text)
+    #Keys of the dictionnary: skip, page_size, products, count, page
+
+    for product in dict_element['products']:
+        print(product)#pylint: disable=C0325
+
+    #Check type of the object need to be dict
+    print(type(dict_element))#pylint: disable=C0325
 
 if __name__ == "__main__":
     main()
