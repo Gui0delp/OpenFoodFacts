73621e97f9836b4162accbc0563e041d65a77d97
Gui0delp <delpro.guillaume@gmail.com>
2019-07-17 20:55:52 +0200

    Minor modification of the file

--- openfoodfacts.py
+++ openfoodfacts.py
@@ -5,18 +5,12 @@
     Docstring
 """
 
-import json
-import requests
+import requests  #pylint: disable=E0401
 
 def main():
     """
         Main function of the script
     """
-
-    req_url = ''
-
-    #Je liste les catégories à recupérer category_list
-    #Je boucle sur la liste
 
     #List of the differents categories from openfoodfacts API
     category_list = [\
@@ -32,14 +26,20 @@ def main():
         "legumes-et-derives"]
 
     #URL https://fr.openfoodfacts.org/categorie/pizza.json
+    print("\nTest the connexions with the differents URL...")# pylint: disable=C0325
+    print("-----------------------------------------------------------------------")# pylint: disable=C0325
     for element in category_list:
 
-        response = requests.get("https://fr.openfoodfacts.org/categorie/{a}.json".format(a = element))
+        quiery_file = requests.get("https://fr.openfoodfacts.org/categorie/{a}.json".format(a=element))# pylint: disable=C0301
 
-        if response.status_code == requests.codes.ok:
-            print("[code: 200] - the category {a} answer well".format(a= element))
+        if quiery_file.status_code == requests.codes.ok:
+            print("[code: 200] - the category {a} answer well".format(a=element))#pylint: disable=C0325
         else:
-            print(response)
+            print(quiery_file)#pylint: disable=C0325
+
+    print("-----------------------------------------------------------------------")#pylint: disable=C0325
+
+    print(quiery_file.json)#pylint: disable=C0325
 
 if __name__ == "__main__":
     main()
