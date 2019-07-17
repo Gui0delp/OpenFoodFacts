44189b748a3d0c449ab4dfe081213a41f3bfcecc
Gui0delp <delpro.guillaume@gmail.com>
2019-07-17 20:11:21 +0200

    Add the module requests

--- openfoodfacts.py
+++ openfoodfacts.py
@@ -5,16 +5,34 @@
     Docstring
 """
 
+import json
+import requests
+
 def main():
     """
         Main function of the script
     """
 
+    req_url = ''
+
     #Je liste les catégories à recupérer category_list
     #Je boucle sur la liste
 
+    #List of the different categories#List of the different categories from openfoodfacts API
+    category_list = [\
+        "conserves",\
+        "pizzas",\
+        "plats",\
+        "yaourts",\
+        "jambons",\
+        "barres-de-cereales",\
+        "sauces",\
+        "pates-alimentaires",\
+        "biscuits",\
+        "legumes-et-derives"]
 
-    pass
+    response = requests.get('https://fr.openfoodfacts.org/categorie/pizza.json')
+    print(response)
 
 if __name__ == "__main__":
     main()
